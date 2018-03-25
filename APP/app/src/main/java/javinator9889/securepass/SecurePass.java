06e104b8701b9ae03f84974568416bb9404f12a3
Javinator9889 <javialonso007@hotmail.es>
2018-03-25 14:24:38 +0200

    Created project and SecurePass class

---
+++ APP/app/src/main/java/javinator9889/securepass/SecurePass.java
@@ -0,0 +1,24 @@
+package javinator9889.securepass;
+
+import android.app.Application;
+import android.support.annotation.NonNull;
+
+/**
+ * Created by Javinator9889 on 25/03/2018.
+ * Main application - class with different parameters useful for app packages
+ */
+
+public class SecurePass extends Application {
+    private static SecurePass APPLICATION_INSTANCE;
+
+    @NonNull
+    public static SecurePass getApplicationInstance() {
+        return APPLICATION_INSTANCE;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        APPLICATION_INSTANCE = this;
+    }
+}
