0c2befad7515f60be6ded3d528324812a55c09ca
Javinator9889 <javialonso007@hotmail.es>
2018-03-27 12:37:23 +0200

    Cipher class created for exporting password

--- APP/app/src/main/java/javinator9889/securepass/Utils/IO/IOManager.java
+++ APP/app/src/main/java/javinator9889/securepass/Utils/IO/IOManager.java
@@ -2,14 +2,31 @@ package javinator9889.securepass.Utils.IO;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.util.Log;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.KeyPair;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.UnrecoverableKeyException;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
 
 import javinator9889.securepass.R;
+import javinator9889.securepass.Utils.Cipher.DataCipher;
+import javinator9889.securepass.Utils.Values.Constants;
 
 /**
  * Created by Javinator9889 on 26/03/2018.
@@ -22,7 +39,7 @@ public class IOManager {
     private File cacheDir;
     private InputStream sqlScriptInputFile;
 
-    private IOManager(Context activityContext) {
+    private IOManager(@NonNull Context activityContext) {
         this.activityContext = activityContext;
         this.filesDir = activityContext.getFilesDir();
         this.cacheDir = activityContext.getCacheDir();
@@ -43,5 +60,77 @@ public class IOManager {
         while ((currentLine = sqlStringsInFile.readLine()) != null)
             completeFileRead.append(currentLine).append("\n");
         return completeFileRead.toString();
+    }
+
+    public void storePassword(@NonNull String userPassword) {
+        try {
+            DataCipher cipher = DataCipher.newInstance(activityContext);
+            cipher.createAndroidAsymmetricKey(Constants.CIPHER.MASTER_KEY);
+            KeyPair masterKey = cipher.getAndroidKeyStoreAsymmetricKeyPair(
+                    Constants.CIPHER.MASTER_KEY);
+            String encryptedPass = cipher.encrypt(userPassword, masterKey.getPublic());
+            storePasswordInFile(encryptedPass);
+        } catch (NoSuchProviderException | NoSuchAlgorithmException |
+                InvalidAlgorithmParameterException | KeyStoreException | UnrecoverableKeyException
+                | BadPaddingException | IllegalBlockSizeException | InvalidKeyException e) {
+            Log.e("PASSWORD-S", "Error while recovering-getting passwords. Message: "
+                    + e.getMessage() + "\nFull trace: ");
+            e.printStackTrace();
+        }
+    }
+
+    private void storePasswordInFile(@NonNull String base64StringPassword) {
+        try {
+            File fileCreator = new File(this.filesDir, Constants.IO.PASS_FILENAME);
+            fileCreator.createNewFile();
+            FileOutputStream outputFile = activityContext.openFileOutput(Constants.IO.PASS_FILENAME,
+                    Context.MODE_PRIVATE);
+            outputFile.write(base64StringPassword.getBytes());
+            outputFile.close();
+        } catch (IOException e) {
+            Log.e("PASSWORD", "Unable to save password in file. Message: " + e.getMessage()
+                    + "\nFull trace: ");
+            e.printStackTrace();
+        }
+    }
+
+    @Nullable
+    public String readPassword() {
+        try {
+            DataCipher cipher = DataCipher.newInstance(activityContext);
+            cipher.createAndroidAsymmetricKey(Constants.CIPHER.MASTER_KEY);
+            KeyPair masterKey = cipher.getAndroidKeyStoreAsymmetricKeyPair(
+                    Constants.CIPHER.MASTER_KEY);
+            return cipher.decrypt(readContentsFromPasswordFile(),
+                    masterKey.getPrivate());
+        } catch (NoSuchProviderException | NoSuchAlgorithmException |
+                InvalidAlgorithmParameterException | KeyStoreException | UnrecoverableKeyException
+                | BadPaddingException | IllegalBlockSizeException | InvalidKeyException e)
+        {
+            Log.e("PASSWORD-S", "Error while recovering-getting passwords. Message: "
+                    + e.getMessage() + "\nFull trace: ");
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    @Nullable
+    private String readContentsFromPasswordFile() {
+        try {
+            InputStream inputPasswordFile = activityContext
+                    .openFileInput(Constants.IO.PASS_FILENAME);
+            BufferedReader passwordReader = new BufferedReader(
+                    new InputStreamReader(inputPasswordFile));
+            StringBuilder completePassword = new StringBuilder();
+            String currentLine;
+            while ((currentLine = passwordReader.readLine()) != null)
+                completePassword.append(currentLine).append("\n");
+            return completePassword.toString();
+        } catch (IOException e) {
+            Log.e("PASSWORD-READ", "Error while trying to read password from file. " +
+                    "Message: " + e.getMessage() + "\nFull trace: ");
+            e.printStackTrace();
+            return null;
+        }
     }
 }
--- APP/app/src/main/java/javinator9889/securepass/Utils/Values/Constants.java
+++ APP/app/src/main/java/javinator9889/securepass/Utils/Values/Constants.java
@@ -1,5 +1,7 @@
 package javinator9889.securepass.Utils.Values;
 
+import android.os.Build;
+
 /**
  * Created by Javinator9889 on 26/03/2018.
  * Constants of the application that will be used in hole program
@@ -9,5 +11,16 @@ public class Constants {
     public static class SQL {
         public static String DB_FILENAME = "SecurePass.db";
         public static String DB_INIT_THREAD_NAME = "DB Initializer";
+    }
+
+    public static class CIPHER {
+        public static String KEYSTORE = "AndroidKeyStore";
+        public static String ALGORITHM = "RSA";
+        public static String TRANSFORMATION = "RSA/ECB/PKCS1Padding";
+        public static String MASTER_KEY = "javinator9889.securepass." + Build.DEVICE;
+    }
+
+    public static class IO {
+        public static String PASS_FILENAME = "userPass.dat";
     }
 }
