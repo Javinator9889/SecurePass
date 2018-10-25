e641dfb365047dabe408e9f40b10391671b48842
Javinator9889 <javialonso007@hotmail.es>
2018-10-25 16:58:21 +0200

    JavaDoc for SignIn class

--- APP/app/src/main/java/javinator9889/securepass/backup/drive/base/SignIn.java
+++ APP/app/src/main/java/javinator9889/securepass/backup/drive/base/SignIn.java
@@ -2,22 +2,22 @@ package javinator9889.securepass.backup.drive.base;
 
 import android.app.Activity;
 import android.content.Context;
-import androidx.annotation.NonNull;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.drive.Drive;
 
+import androidx.annotation.NonNull;
 import javinator9889.securepass.objects.GeneralObjectContainer;
 import javinator9889.securepass.objects.ObjectContainer;
 
 /**
+ * Singleton class for singing-in into Google Drive
  * Created by Javinator9889 on 25/04/2018.
  */
 public class SignIn {
     private static final int REQUEST_CODE_SIGN_IN = 0;
-
     private static SignIn INSTANCE = null;
     private GeneralObjectContainer<Activity> mActivityContainer;
     private GeneralObjectContainer<Context> mContextContainer;
@@ -25,6 +25,20 @@ public class SignIn {
     private boolean mIsSignInClientBuilt;
     private GoogleSignInClient mGoogleSignInClient;
 
+    /**
+     * Generates the <code>SignIn</code> instance the first time it is executed
+     * <p>
+     * Stores <code>Activity</code> and <code>Context</code> into
+     * <code>ObjectContainer</code> for avoiding memory leaks
+     * </p>
+     *
+     * @param mainActivity <code>Activity</code> when the class was launched
+     * @param driveContext <code>Context</code> when the class was launched
+     * @see Activity
+     * @see Context
+     * @see ObjectContainer
+     * @see GeneralObjectContainer
+     */
     private SignIn(@NonNull Activity mainActivity, @NonNull Context driveContext) {
         INSTANCE = this;
         this.mActivityContainer = new ObjectContainer<>(mainActivity);
@@ -33,11 +47,32 @@ public class SignIn {
         this.mIsSignInClientBuilt = false;
     }
 
+    /**
+     * Accessible method for obtaining a <code>SignIn</code> class instance
+     * <p>
+     * Stores <code>Activity</code> and <code>Context</code> into
+     * <code>ObjectContainer</code> for avoiding memory leaks
+     * </p>
+     *
+     * @param mainActivity <code>Activity</code> when the class was launched
+     * @param driveContext <code>Context</code> when the class was launched
+     * @return SignIn instance if available, else new instance
+     * @see Activity
+     * @see Context
+     * @see ObjectContainer
+     * @see GeneralObjectContainer
+     */
     public static SignIn getInstance(@NonNull Activity mainActivity,
                                      @NonNull Context driveContext) {
         return INSTANCE != null ? INSTANCE : new SignIn(mainActivity, driveContext);
     }
 
+    /**
+     * Signs-in into Google Drive by requesting the user some credentials
+     *
+     * @see GoogleSignInClient
+     * @see #buildGoogleSignInClient()
+     */
     public void signIn() {
         Activity mainActivity = mActivityContainer.getLatestStoredObject();
         if (mIsSignInClientBuilt)
@@ -50,6 +85,13 @@ public class SignIn {
         }
     }
 
+    /**
+     * Generates the Google Drive sign in client, necessary for accessing to the Google Drive API
+     * at the app folder with the chosen account
+     *
+     * @see GoogleSignIn
+     * @see GoogleSignInOptions
+     */
     private void buildGoogleSignInClient() {
         Context driveContext = mContextContainer.getLatestStoredObject();
         GoogleSignInOptions signInOptions =
@@ -60,14 +102,27 @@ public class SignIn {
         mIsSignInClientBuilt = true;
     }
 
+    /**
+     * Sets whether the user is or not signed in
+     *
+     * @param isSignedIn <code>boolean</code> 'true' if already signed-in, else 'false'
+     */
     public void setSignedIn(boolean isSignedIn) {
         this.mIsSignedIn = isSignedIn;
     }
 
+    /**
+     * Checks if user is already signed-in
+     *
+     * @return boolean 'true' when signed-in, else 'false'
+     */
     public boolean isSignedIn() {
         return mIsSignedIn;
     }
 
+    /**
+     * Finish this class when a new instance is required for some reason
+     */
     public void finish() {
         mIsSignedIn = false;
         mIsSignInClientBuilt = false;
