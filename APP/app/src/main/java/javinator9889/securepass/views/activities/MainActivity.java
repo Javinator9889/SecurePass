f522e3b60e9a7c752e726d2b618ec5654db1e32e
Javinator9889 <javialonso007@hotmail.es>
2018-10-25 10:21:29 +0200

    Included entries inside the MainActivity

--- APP/app/src/main/java/javinator9889/securepass/views/activities/MainActivity.java
+++ APP/app/src/main/java/javinator9889/securepass/views/activities/MainActivity.java
@@ -5,25 +5,39 @@ import android.os.Bundle;
 import android.view.View;
 
 import com.mikepenz.material_design_iconic_typeface_library.MaterialDesignIconic;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItem;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItems;
 
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.res.ResourcesCompat;
 import javinator9889.securepass.R;
+import javinator9889.securepass.views.TabLayoutAdapter;
 import javinator9889.securepass.views.activities.menus.FloatingActionButtonMenuAdapter;
+import javinator9889.securepass.views.fragments.EntriesDisplayer;
 import uk.co.markormesher.android_fab.FloatingActionButton;
 
 /**
  * Created by Javinator9889 on 11/10/2018.
  */
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends TabLayoutAdapter implements View.OnClickListener {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
         setContentView(R.layout.main_view);
+        setTabIdRes(R.id.main_view_tabs);
+        setViewPagerIdRes(R.id.main_viewpager);
+        setSelectedTextFontRes(R.font.raleway_semibold);
+        setNonSelectedTextFontRes(R.font.raleway);
+        setMustChangeTypeface(true);
+        setMustShowBackButtonOnActionBar(true);
+        super.onCreate(savedInstanceState);
     }
 
-
+    @Override
+    protected FragmentPagerItems getItems() {
+        FragmentPagerItems items = new FragmentPagerItems(this);
+        items.add(FragmentPagerItem.of("Title test", EntriesDisplayer.class));
+        return items;
+    }
 
     @Override
     protected void onStart() {
