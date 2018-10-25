09752b3d2a8ad70d320412a4784a8c9a3f1d2112
Javinator9889 <javialonso007@hotmail.es>
2018-10-25 10:12:16 +0200

    Optimized code for tabs - created abstract class that avoids repeating code

--- APP/app/src/main/java/javinator9889/securepass/views/activities/MainActivity.java
+++ APP/app/src/main/java/javinator9889/securepass/views/activities/MainActivity.java
@@ -1,31 +1,17 @@
 package javinator9889.securepass.views.activities;
 
-import android.content.Context;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.os.PersistableBundle;
-import android.util.AttributeSet;
 import android.view.View;
-import android.widget.TextView;
 
-import com.mikepenz.iconics.IconicsDrawable;
-import com.mikepenz.iconics.context.IconicsContextWrapper;
-import com.mikepenz.iconics.context.IconicsLayoutInflater2;
 import com.mikepenz.material_design_iconic_typeface_library.MaterialDesignIconic;
 
-import org.jetbrains.annotations.NotNull;
-
 import androidx.annotation.Nullable;
-
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.res.ResourcesCompat;
-import androidx.core.view.LayoutInflaterCompat;
 import javinator9889.securepass.R;
 import javinator9889.securepass.views.activities.menus.FloatingActionButtonMenuAdapter;
 import uk.co.markormesher.android_fab.FloatingActionButton;
-import uk.co.markormesher.android_fab.SpeedDialMenuAdapter;
-import uk.co.markormesher.android_fab.SpeedDialMenuItem;
 
 /**
  * Created by Javinator9889 on 11/10/2018.
@@ -33,15 +19,14 @@ import uk.co.markormesher.android_fab.SpeedDialMenuItem;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
-        System.out.println("On Create");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_view);
-        System.out.println("Finished on create");
     }
+
+
 
     @Override
     protected void onStart() {
-        System.out.println("On Start");
         FloatingActionButton button = findViewById(R.id.fab);
         button.setOnClickListener(this);
         button.setContentCoverEnabled(true);
@@ -57,13 +42,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 .withIconRotationEnabled(true)
                 .build();
         button.setSpeedDialMenuAdapter(menuAdapter);
-        System.out.println("Created fab");
         super.onStart();
     }
 
     @Override
     public void onClick(View v) {
-        System.out.println("Clicked!");
         switch (v.getId()) {
             case R.id.fab:
                 FloatingActionButton button = v.findViewById(R.id.fab);
--- APP/app/src/main/java/javinator9889/securepass/views/activities/ShowEulaActivity.java
+++ APP/app/src/main/java/javinator9889/securepass/views/activities/ShowEulaActivity.java
@@ -1,28 +1,15 @@
 package javinator9889.securepass.views.activities;
 
-import android.graphics.Typeface;
 import android.os.Bundle;
-import androidx.annotation.Nullable;
-import androidx.annotation.StringRes;
-import androidx.appcompat.app.ActionBar;
-import androidx.core.content.res.ResourcesCompat;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentPagerAdapter;
-import androidx.viewpager.widget.ViewPager;
-import androidx.appcompat.app.AppCompatActivity;
-import android.view.MenuItem;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 
-import com.ogaclejapan.smarttablayout.SmartTabLayout;
 import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItem;
-import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItemAdapter;
 import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItems;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-
+import androidx.annotation.Nullable;
+import androidx.annotation.StringRes;
+import androidx.fragment.app.Fragment;
 import javinator9889.securepass.R;
+import javinator9889.securepass.views.TabLayoutAdapter;
 import javinator9889.securepass.views.fragments.termsofservice.LicenseFragment;
 import javinator9889.securepass.views.fragments.termsofservice.PrivacyFragment;
 import javinator9889.securepass.views.fragments.termsofservice.TermsFragment;
@@ -30,97 +17,30 @@ import javinator9889.securepass.views.fragments.termsofservice.TermsFragment;
 /**
  * Created by Javinator9889 on 17/04/2018.
  */
-public class ShowEulaActivity extends AppCompatActivity
-        implements SmartTabLayout.OnTabClickListener, ViewPager.OnPageChangeListener {
-    private int currentPosition = 0;
-    private ViewPager viewPager;
-    private SmartTabLayout tabLayout;
-
+public class ShowEulaActivity extends TabLayoutAdapter {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
         setContentView(R.layout.full_eula);
-        FragmentPagerItem[] availableItems = getItems();
-        FragmentPagerItems.Creator itemsCreator = FragmentPagerItems.with(this);
-        for (FragmentPagerItem item : availableItems)
-            itemsCreator.add(item);
-        FragmentPagerAdapter adapter = new FragmentPagerItemAdapter(getSupportFragmentManager(),
-                itemsCreator.create());
-        viewPager = findViewById(R.id.viewpager);
-        viewPager.setAdapter(adapter);
-        tabLayout = findViewById(R.id.viewpagertab);
-        tabLayout.setViewPager(viewPager);
-        tabLayout.setOnTabClickListener(this);
-        tabLayout.setOnPageChangeListener(this);
-        ActionBar supportActionBar = getSupportActionBar();
-        if (supportActionBar != null)
-            supportActionBar.setDisplayHomeAsUpEnabled(true);
+        setTabIdRes(R.id.viewpagertab);
+        setViewPagerIdRes(R.id.viewpager);
+        setSelectedTextFontRes(R.font.raleway_semibold);
+        setNonSelectedTextFontRes(R.font.raleway);
+        setMustChangeTypeface(true);
+        setMustShowBackButtonOnActionBar(true);
+        super.onCreate(savedInstanceState);
     }
 
+    @Override
     @SuppressWarnings("unchecked")
-    private FragmentPagerItem[] getItems() {
-        Class<? extends Fragment>[] availableItems =
-                new Class[]{PrivacyFragment.class, TermsFragment.class, LicenseFragment.class};
-        @StringRes int[] titles =
-                new int[]{R.string.privacy_name, R.string.tos_name, R.string.eula_name};
-        final int count = availableItems.length;
-        FragmentPagerItem[] items = new FragmentPagerItem[count];
-        for (int i = 0; i < count; ++i) {
-            items[i] = FragmentPagerItem.of(getString(titles[i]), availableItems[i]);
-        }
+    protected FragmentPagerItems getItems() {
+        FragmentPagerItems items = new FragmentPagerItems(this);
+        Class<? extends Fragment>[] availableItems = new Class[]{PrivacyFragment.class,
+                TermsFragment.class, LicenseFragment.class};
+        @StringRes int[] availableTitles = new int[]{R.string.privacy_name, R.string.tos_name,
+                R.string.eula_name};
+        final int itemsCount = availableItems.length;
+        for (int i = 0; i < itemsCount; ++i)
+            items.add(FragmentPagerItem.of(getString(availableTitles[i]), availableItems[i]));
         return items;
-    }
-
-    @Override
-    public void onBackPressed() {
-        if (currentPosition > 0) {
-            --currentPosition;
-            viewPager.setCurrentItem(currentPosition, true);
-        } else
-            super.onBackPressed();
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == android.R.id.home) {
-            currentPosition = 0;
-            onBackPressed();
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    private void changeTextTypeface(int position) {
-        Typeface nonSelectedText = ResourcesCompat.getFont(this, R.font.raleway);
-        Typeface selectedText = ResourcesCompat.getFont(this, R.font.raleway_semibold);
-        LinearLayout tabs = (LinearLayout) tabLayout.getChildAt(0);
-        for (int i = 0; i < tabs.getChildCount(); ++i) {
-            TextView currentText = (TextView) tabs.getChildAt(i);
-            if (i != position)
-                currentText.setTypeface(nonSelectedText);
-            else
-                currentText.setTypeface(selectedText);
-        }
-    }
-
-    @Override
-    public void onTabClicked(int position) {
-        this.currentPosition = position;
-        changeTextTypeface(position);
-    }
-
-    @Override
-    public void onPageScrolled(int i, float v, int i1) {
-        return;
-    }
-
-    @Override
-    public void onPageSelected(int currentPosition) {
-        this.currentPosition = currentPosition;
-        changeTextTypeface(currentPosition);
-    }
-
-    @Override
-    public void onPageScrollStateChanged(int i) {
-        return;
     }
 }
