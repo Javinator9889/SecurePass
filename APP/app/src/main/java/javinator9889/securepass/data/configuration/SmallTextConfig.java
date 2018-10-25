4cff4317e29f3c701c93a73876572fffb13f10f7
Javinator9889 <javialonso007@hotmail.es>
2018-10-25 18:08:02 +0200

    JavaDoc for *Config classes

--- APP/app/src/main/java/javinator9889/securepass/data/configuration/SmallTextConfig.java
+++ APP/app/src/main/java/javinator9889/securepass/data/configuration/SmallTextConfig.java
@@ -2,8 +2,6 @@ package javinator9889.securepass.data.configuration;
 
 import androidx.annotation.Nullable;
 
-import java.io.Serializable;
-
 import javinator9889.securepass.util.values.Constants.SQL.SMALL_TEXT_CONFIG;
 import javinator9889.securepass.util.values.DatabaseTables;
 
@@ -14,16 +12,42 @@ public class SmallTextConfig extends ConfigFields {
     private static final String TABLE_NAME = SMALL_TEXT_CONFIG.NAME;
     private static final DatabaseTables TABLE_TYPE = DatabaseTables.SMALL_TEXT;
 
+    /**
+     * Constructor that uses
+     * {@link ConfigFields#ConfigFields(long, String, int, long) ConfigFileds constructor} (only
+     * visible for this classes) - generates SmallTextConfig instance
+     *
+     * @param id          config field id
+     * @param description optional description
+     * @param sortOrder   fields sort order
+     * @param configId    configuration id
+     * @see ConfigFields
+     * @see IConfigFields
+     */
     public SmallTextConfig(long id, @Nullable String description, int sortOrder,
                            long configId) {
         super(id, description, sortOrder, configId);
     }
 
+    /**
+     * Gets the proper table name - names available at
+     * {@link javinator9889.securepass.util.values.Constants.SQL SQL names} attributes
+     *
+     * @return String which is the table name
+     * @see javinator9889.securepass.util.values.Constants.SQL
+     */
     @Override
     public String getTableName() {
         return TABLE_NAME;
     }
 
+    /**
+     * Gets the proper database type - types available at
+     * {@link DatabaseTables DatabaseTables enum} types
+     *
+     * @return DatabaseTables object with the correspondent type
+     * @see DatabaseTables
+     */
     @Override
     public DatabaseTables getTableType() {
         return TABLE_TYPE;
