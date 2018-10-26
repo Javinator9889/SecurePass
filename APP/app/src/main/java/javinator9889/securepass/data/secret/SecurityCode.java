491a0b049e64b2c4356b70b31ce58602fb6ccf10
Javinator9889 <javialonso007@hotmail.es>
2018-10-26 17:43:51 +0200

    JavaDoc for SecurityCode class

--- APP/app/src/main/java/javinator9889/securepass/data/secret/SecurityCode.java
+++ APP/app/src/main/java/javinator9889/securepass/data/secret/SecurityCode.java
@@ -5,27 +5,44 @@ import androidx.annotation.NonNull;
 import java.io.Serializable;
 
 /**
+ * Class that contains Fields with SecurityCodes
  * Created by Javinator9889 on 29/03/2018.
  */
 public class SecurityCode implements Serializable {
-    private int id;
-    private String accountName;
+    private long mId;
+    private String mAccountName;
 
-    public SecurityCode(int id, @NonNull String accountName) {
-        this.id = id;
-        this.accountName = accountName;
+    /**
+     * Public available constructor for SecurityCodes
+     *
+     * @param id          SecurityCode ID
+     * @param accountName SecurityCode name
+     */
+    public SecurityCode(long id, @NonNull String accountName) {
+        this.mId = id;
+        this.mAccountName = accountName;
     }
 
-    public int getId() {
-        return id;
+    /**
+     * Obtains current ID
+     *
+     * @return <code>long</code> with the ID
+     */
+    public long getId() {
+        return mId;
     }
 
+    /**
+     * Obtains current SecurityCode account name
+     * @return <code>String</code> with the name
+     */
     public String getAccountName() {
-        return accountName;
+        return mAccountName;
     }
 
     @Override
     public String toString() {
-        return "SecurityCode ID: " + id + "\nSecurityCode account name: " + accountName + "\n";
+        return "SecurityCode ID: " + mId +
+                "\nSecurityCode account name: " + mAccountName + "\n";
     }
 }
