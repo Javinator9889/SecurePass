465dd43e5853c2577821c1fd1bd5b3eb561f89eb
Javinator9889 <javialonso007@hotmail.es>
2018-10-26 18:19:02 +0200

    JavaDoc for DatabaseManager class

--- APP/app/src/main/java/javinator9889/securepass/io/database/DatabaseManager.java
+++ APP/app/src/main/java/javinator9889/securepass/io/database/DatabaseManager.java
@@ -1,7 +1,6 @@
 package javinator9889.securepass.io.database;
 
 import android.content.Context;
-import androidx.annotation.NonNull;
 import android.util.Log;
 
 import net.sqlcipher.database.SQLiteDatabase;
@@ -10,10 +9,10 @@ import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
+import androidx.annotation.NonNull;
 import javinator9889.securepass.io.IOManager;
 import javinator9889.securepass.util.resources.ISharedPreferencesManager;
 import javinator9889.securepass.util.resources.PreferencesManager;
-import javinator9889.securepass.util.resources.SharedPreferencesManager;
 import javinator9889.securepass.util.values.Constants;
 
 /**
@@ -22,44 +21,76 @@ import javinator9889.securepass.util.values.Constants;
  */
 
 public class DatabaseManager {
-    private Context activityContext;
-    private File databaseFile;
-    private String userHashedPassword;
-    private Thread databaseInitializer;
+    private Context mActivityContext;
+    private File mDatabaseFile;
+    private String mUserHashedPassword;
+    private Thread mDatabaseInitializer;
 
+    /**
+     * Generates a new instance and also initializes the database with {@link #initDB()} method
+     *
+     * @param activityContext    <code>Context</code> when instantiating the class
+     * @param userHashedPassword password for encrypting/decrypting the database
+     * @see Context
+     * @see #initDB()
+     */
+    private DatabaseManager(Context activityContext, @NonNull String userHashedPassword) {
+        this.mActivityContext = activityContext;
+        this.mUserHashedPassword = userHashedPassword;
+        initDB();
+    }
+
+    /**
+     * Generates a <code>DatabaseManager</code> instance
+     *
+     * @param activityContext    <code>Context</code> when instantiating the class
+     * @param userHashedPassword password for encrypting/decrypting the database
+     * @return <code>DatabaseManager</code> instance
+     * @see Context
+     */
     @NonNull
     public static DatabaseManager newInstance(Context activityContext,
                                               @NonNull String userHashedPassword) {
         return new DatabaseManager(activityContext, userHashedPassword);
     }
 
-    private DatabaseManager(Context activityContext, @NonNull String userHashedPassword) {
-        this.activityContext = activityContext;
-        this.userHashedPassword = userHashedPassword;
-        initDB();
-    }
-
+    /**
+     * Initializes de database with the required script on a <b>separate thread</b>.
+     * <p>
+     * It reads from {@link ISharedPreferencesManager} if the
+     * {@link ISharedPreferencesManager#isApplicationInitialized() application is initialized
+     * } for creating the correspondent files. <br />
+     * If the
+     * {@link ISharedPreferencesManager#isDatabaseInitialized() database is initialized} it
+     * will not load again the scripts. Else, by using {@link IOManager} it will create the
+     * tables inside SQLite
+     * </p>
+     *
+     * @see SQLiteDatabase
+     * @see PreferencesManager
+     * @see IOManager#loadSQLScript()
+     */
     private void initDB() {
-        databaseInitializer = new Thread(new Runnable() {
-            private final Context databaseContext = DatabaseManager.this.activityContext;
-            private final String databasePassword = DatabaseManager.this.userHashedPassword;
+        mDatabaseInitializer = new Thread(new Runnable() {
+            private final Context databaseContext = DatabaseManager.this.mActivityContext;
+            private final String databasePassword = DatabaseManager.this.mUserHashedPassword;
 
             @Override
             public void run() {
                 SQLiteDatabase.loadLibs(databaseContext);
                 ISharedPreferencesManager preferencesManager = PreferencesManager.getInstance();
-                databaseFile = databaseContext
+                mDatabaseFile = databaseContext
                         .getDatabasePath(Constants.SQL.DB_FILENAME);
                 if (!preferencesManager.isApplicationInitialized()) {
                     try {
-                        databaseFile.delete();
-                        databaseFile.createNewFile();
+                        mDatabaseFile.delete();
+                        mDatabaseFile.createNewFile();
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
                 }
                 SQLiteDatabase database = SQLiteDatabase.openOrCreateDatabase(
-                        databaseFile,
+                        mDatabaseFile,
                         databasePassword,
                         null);
                 List<String> databaseScripts;
@@ -78,33 +109,53 @@ public class DatabaseManager {
                 }
             }
         });
-        databaseInitializer.setName(Constants.SQL.DB_INIT_THREAD_NAME);
-        databaseInitializer.setUncaughtExceptionHandler(new ThreadExceptionHandler());
-        databaseInitializer.run();
+        mDatabaseInitializer.setName(Constants.SQL.DB_INIT_THREAD_NAME);
+        mDatabaseInitializer.setUncaughtExceptionHandler(new ThreadExceptionHandler());
+        mDatabaseInitializer.run();
     }
 
+    /**
+     * Gets the database instance from SQLite
+     *
+     * @return <code>SQLiteDatabase</code> instance
+     */
     public SQLiteDatabase getDatabaseInstance() {
-        SQLiteDatabase.loadLibs(activityContext);
-        return SQLiteDatabase.openOrCreateDatabase(databaseFile, userHashedPassword, null);
+        SQLiteDatabase.loadLibs(mActivityContext);
+        return SQLiteDatabase.openOrCreateDatabase(mDatabaseFile, mUserHashedPassword, null);
     }
 
+    /**
+     * Gets the thread when running for joining the database initializer with current thread
+     *
+     * @return <code>Thread</code> with the init process
+     * @see Thread
+     * @see Thread#join()
+     */
     public Thread getDatabaseInitializer() {
-        return databaseInitializer;
+        return mDatabaseInitializer;
     }
 
+    /**
+     * Creates the default category necessary in order to allow the hole application work
+     */
+    private void createDefaultCategory() {
+        DatabaseOperations operations = DatabaseOperations.newInstance(this);
+        operations.registerDefaultCategory();
+        operations.finishConnection();
+    }
+
+    /**
+     * Class for handling <code>Threading</code> exceptions
+     */
     private class ThreadExceptionHandler implements Thread.UncaughtExceptionHandler {
+        /**
+         * {@inheritDoc}
+         */
         @Override
         public void uncaughtException(Thread t, Throwable e) {
             Log.e(t.getName(), "Exception in thread \"" + t.getName() + "\" with " +
                     "exception thrown -> " + e.getMessage() + "\nFull trace: ");
             e.printStackTrace();
         }
-    }
-
-    private void createDefaultCategory() {
-        DatabaseOperations operations = DatabaseOperations.newInstance(this);
-        long id = operations.registerDefaultCategory();
-        Log.d("DB", "Category ID: " + id);
-        operations.finishConnection();
     }
 }
