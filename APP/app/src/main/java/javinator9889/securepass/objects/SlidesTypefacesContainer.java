5d9a4b4ca445f23c798a40e289a390179d4ac0ce
Javinator9889 <javialonso007@hotmail.es>
2018-10-27 17:57:18 +0200

    JavaDoc for Container classes
    Removed unused ones

--- APP/app/src/main/java/javinator9889/securepass/objects/SlidesTypefacesContainer.java
+++ APP/app/src/main/java/javinator9889/securepass/objects/SlidesTypefacesContainer.java
@@ -3,6 +3,8 @@ package javinator9889.securepass.objects;
 import androidx.annotation.FontRes;
 
 /**
+ * Class for containing <code>Typeface</code> for
+ * {@link com.github.paolorotolo.appintro.model.SliderPage}
  * Created by Javinator9889 on 12/10/2018.
  */
 public class SlidesTypefacesContainer {
@@ -11,16 +13,35 @@ public class SlidesTypefacesContainer {
     private @FontRes
     int mDescriptionTypeface;
 
+    /**
+     * Public available constructor for saving the <code>Typeface</code>s
+     *
+     * @param titleTypeface       title custom typeface (must be a @FontRes)
+     * @param descriptionTypeface description custom typeface (must be a @FontRes)
+     * @see FontRes
+     */
     public SlidesTypefacesContainer(@FontRes int titleTypeface, @FontRes int descriptionTypeface) {
         this.mTitleTypeface = titleTypeface;
         this.mDescriptionTypeface = descriptionTypeface;
     }
 
+    /**
+     * Obtains the stored title <code>Typeface</code>
+     *
+     * @return <code>int</code> with the @FontRes typeface
+     * @see FontRes
+     */
     @FontRes
     public int getTitleTypeface() {
         return mTitleTypeface;
     }
 
+    /**
+     * Obtains the stored description <code>Typeface</code>
+     *
+     * @return <code>int</code> with the @FontRes typeface
+     * @see FontRes
+     */
     @FontRes
     public int getDescriptionTypeface() {
         return mDescriptionTypeface;
